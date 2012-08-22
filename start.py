3a1751c61229e5141fa3cda0be6fae0bec7450fd
Michael Hernandez <michael.hernandez1988@gmail.com>
2012-08-22 13:04:30 -0400

    tor|mongo

--- start.py
+++ start.py
@@ -5,10 +5,20 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class FindZaHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self):
+        conn = pymongo.Connection('localhost', 27017)
+        db = connection.za
+
+        print db.za.find()
+
 application = tornado.web.Application([
     (r"/", MainHandler),
-    (r"/asynctest", async_mongo_test.AsyncTestHandler),
+    (r"/get/za", async_mongo_test.FindZaHandler),
 ])
+
 
 if __name__ == "__main__":
     application.listen(8888)
--- start_mongo.py
+++ start_mongo.py
@@ -1,16 +1,11 @@
-import asyncmongo
+import pymongo
 import tornado.web
 
 class Handler(tornado.web.RequestHandler):
 
     @tornado.web.asynchronous
     def get(self):
-        db = asyncmongo.Client(pool_id='mypool', host='localhost', 
-            port=27107, dbname='za')
+        conn = pymongo.Connection('localhost', 27017)
+        db = connection.za
 
-        db.za.find(callback=self._on_response)
-
-    def _on_response(self, response, error):
-        if error:
-            raise tornado.web.HTTPError(500)
-        self.render('template', first_name=response['first_name'])
+        print db.za.find()
