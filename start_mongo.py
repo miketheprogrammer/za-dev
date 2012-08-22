e342dcf1ee45c6e8a363a9b0f9ce7457f359bace
Michael Hernandez <michael.hernandez1988@gmail.com>
2012-08-22 12:38:54 -0400

    tor|mongo

---
+++ start_mongo.py
@@ -0,0 +1,16 @@
+import asyncmongo
+import tornado.web
+
+class Handler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self):
+        db = asyncmongo.Client(pool_id='mypool', host='localhost', 
+            port=27107, dbname='za')
+
+        db.users.find(callback=self._on_response)
+
+    def _on_response(self, response, error):
+        if error:
+            raise tornado.web.HTTPError(500)
+        self.render('template', first_name=response['first_name'])
